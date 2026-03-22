2de681c4ac844cc9ef275f0f19bc08309795a050
Sujit Patel <202501987+sujitpatel2739@users.noreply.github.com>
2026-03-22 17:36:25 +0530

    Update Nodes.py

--- backend/neural_network/Nodes/Nodes.py
+++ backend/neural_network/Nodes/Nodes.py
@@ -104,7 +104,6 @@ class MulNode(Node):
         out = self._state['out']
         return out.backward(grad)
 
-
 class DivNode(Node):
     """
     t1 / t2  (exactly two inputs).
@@ -115,19 +114,20 @@ class DivNode(Node):
     dL/dt2 = -grad * t1 / t2^2
     """
 
+    def __init__(self, node_id: int, name: str = ""):
+        super().__init__(node_id, name)
+
     def _forward(self, *inputs: Tensor) -> Tensor:
         self._require_n_inputs(inputs, 2)
         t1, t2 = inputs
-        requires_grad = t1.requires_grad or t2.requires_grad
-        return self._wrap(t1.data / t2.data, requires_grad)
+        out = t1 / t2
+        self._state['inputs'] = [t1, t2]
+        self._state['out']    = out
+        return out
 
     def _backward(self, grad: Tensor) -> List[Tensor]:
-        t1, t2 = self._inputs
-        return [
-            self._wrap( grad.data / t2.data,                    t1.requires_grad),
-            self._wrap(-grad.data * t1.data / (t2.data ** 2),   t2.requires_grad),
-        ]
-
+        out = self._state['out']
+        return out.backward(grad)
 
 class SqNode(Node):
     """
